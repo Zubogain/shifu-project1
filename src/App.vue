96ff2ac527691c2a74398794752b032aff0fdf2d
Zubogain <zubogain@gmail.com>
2018-06-16 15:09:49 +0600

    First commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="hello">
+        <h1>{{ message }}</h1>
+        <p>{{ lorem }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                message: "{Шергали Эмиль} запустил проект, какой он молодец!",
+                lorem: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+            }
+        }
+    }
+</script>
+
+<style>
+    .hello > p {
+        font-size: 2em;
+        color: rebeccapurple;
+    }
+</style>
