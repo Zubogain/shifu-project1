6532e4e1b2b78293d436517654b7b3d850d8e12e
Zubogain <zubogain@gmail.com>
2018-06-22 17:22:31 +0600

    HARD - refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = {
             },
             {
                 test: /\.vue$/,
-                use: 'vue-loader'
+                loader: 'vue-loader'
             }
         ]
     },
