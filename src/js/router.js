857597f6d5fd92ebbe1edaeda374ff3247129648
Zubogain <zubogain@gmail.com>
2018-06-28 13:10:34 +0600

    Light#1 - Bug Fix

--- src/js/router.js
+++ src/js/router.js
@@ -1,7 +1,6 @@
 import Main from './components/Main.vue';
 import List from './components/List.vue';
 import InsertItem from './components/InsertItem.vue';
-import EditItem from  './components/EditItem.vue';
 import PageNotFound from './components/PageNotFound.vue';
 const routes = [
     {
@@ -18,11 +17,6 @@ const routes = [
                 path: 'add',
                 name: 'insertItem',
                 component: InsertItem
-            },
-            {
-                path: 'edit/:id',
-                name: 'editItem',
-                component: EditItem
             }
         ]
     },
